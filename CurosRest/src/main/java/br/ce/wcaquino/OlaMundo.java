342d11dd6fb4645345496bee42942cef10967f85
Amauri Mann <amaurimoraismann@gmail.com>
2020-09-02 00:37:50 -0300

    test Add new system out

--- CurosRest/src/main/java/br/ce/wcaquino/OlaMundo.java
+++ CurosRest/src/main/java/br/ce/wcaquino/OlaMundo.java
@@ -5,9 +5,10 @@ import io.restassured.http.Method;
 import io.restassured.response.Response;
 
 public class OlaMundo {
-	
+
 	public static void main(String[] args) {
 		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
 		System.out.println(response.getBody().asString());
+	System.out.println();
 	}
 }
