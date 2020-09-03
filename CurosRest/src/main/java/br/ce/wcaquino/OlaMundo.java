d0c6ad51caf16c121bd207a8fd326b229955fa22
Amauri Mann <amaurimoraismann@gmail.com>
2020-09-02 23:32:21 -0300

    Primeiro Teste

--- CurosRest/src/main/java/br/ce/wcaquino/OlaMundo.java
+++ CurosRest/src/main/java/br/ce/wcaquino/OlaMundo.java
@@ -3,17 +3,22 @@ package br.ce.wcaquino;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class OlaMundo {
 
 	public static void main(String[] args) {
-		//Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		// Fazendo a requisição
 		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me:80/ola");
 
-		System.out.println(response.getBody().asString());
-		// Status code de respostas HTTP Lista https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
-		System.out.println(response.statusCode());
-		
+		// Coletando respostas Status code de respostas HTTP Lista
+		// https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
 
+		// Validando Estatus Code
+		System.out.println(response.statusCode() == 200);
+
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
 	}
 }
