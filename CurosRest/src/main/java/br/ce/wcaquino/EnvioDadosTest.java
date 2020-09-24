a8c69a42ada94570639376b7d9d3a2a2fe6f04e5
Amauri Mann <amaurimoraismann@gmail.com>
2020-09-23 22:42:27 -0300

    Query parametrizada

--- CurosRest/src/main/java/br/ce/wcaquino/EnvioDadosTest.java
+++ CurosRest/src/main/java/br/ce/wcaquino/EnvioDadosTest.java
@@ -1,7 +1,9 @@
 package br.ce.wcaquino;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,9 +24,25 @@ public class EnvioDadosTest {
 			.statusCode(200)
 			.contentType(ContentType.JSON)
 			
-		;
-		
+		;	
+	}
 	
+	@Test
+	public void deveEnviarValorViaQueryViaParametro() {
+		
+		given()
+			.log().all()
+			.queryParam("format", "xml")
+			.queryParam("outra", "coisa")
+		.when()
+			.get("http://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+			.contentType(containsString("utf-8"))
+			
+		;
 		
 	}
 
