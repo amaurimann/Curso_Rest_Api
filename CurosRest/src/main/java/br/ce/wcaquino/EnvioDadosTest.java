024ba6f86037c7cc5592fc33a997b7ad7977d515
Amauri Mann <amaurimoraismann@gmail.com>
2020-09-24 23:17:01 -0300

    Acessando valores via Header

--- CurosRest/src/main/java/br/ce/wcaquino/EnvioDadosTest.java
+++ CurosRest/src/main/java/br/ce/wcaquino/EnvioDadosTest.java
@@ -45,5 +45,21 @@ public class EnvioDadosTest {
 		;
 		
 	}
+	
+	@Test
+	public void deveEnviarValorViaHeader() {
+		
+		given()
+			.log().all()
+			.accept(ContentType.XML)
+		.when()
+			.get("http://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+			
+		;	
+	}
 
 }
