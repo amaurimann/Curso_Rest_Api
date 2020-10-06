304fcce4985d5367654b17ad61d576a7beaea134
Amauri Mann <amaurimoraismann@gmail.com>
2020-10-06 17:31:33 -0300

    Validando Schema Json

--- CurosRest/src/main/java/br/ce/wcaquino/SchemaTest.java
+++ CurosRest/src/main/java/br/ce/wcaquino/SchemaTest.java
@@ -5,6 +5,8 @@ import static io.restassured.RestAssured.*;
 import org.junit.Test;
 
 import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
 import org.xml.sax.SAXParseException;
 
 
@@ -34,6 +36,21 @@ public class SchemaTest {
 			.log().all()
 			.statusCode(200)
 			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
+		;
+		
+	}
+	
+	
+	@Test
+	public void deveValidarSchemaJson() {
+		given()
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("users.json"))
 		;
 		
 	}
