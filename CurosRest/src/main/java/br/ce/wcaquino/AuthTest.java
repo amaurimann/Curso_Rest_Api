ef7f92880fddb6f3cd640c8d71ce8216e6d295ae
Amauri Mann <amaurimoraismann@gmail.com>
2020-10-06 23:32:46 -0300

    Autenticando usuários

--- CurosRest/src/main/java/br/ce/wcaquino/AuthTest.java
+++ CurosRest/src/main/java/br/ce/wcaquino/AuthTest.java
@@ -48,5 +48,72 @@ public class AuthTest {
 		;
 		
 	}
+	
+	
+	@Test
+	public void naoDeveAcessarSemSenha() {
+		
+		given()
+		.log().all()
+	.when()
+		.get("http://restapi.wcaquino.me/basicauth")
+	.then()
+		.log().all()
+		.statusCode(401)//Status 401 = não autorizado
+		
+		;	
+		
+	}
+	
+	
+	@Test
+	public void DeveFazerAutenticacaoBasica() {
+		
+		given()
+		.log().all()
+	.when()
+		.get("http://admin:senha@restapi.wcaquino.me/basicauth")//passando usuario e senha para acesso no browser
+	.then()
+		.log().all()
+		.statusCode(200)
+		.body("status", is("logado"))
+		
+		;	
+		
+	}
+	
+	@Test
+	public void DeveFazerAutenticacaoBasica2() {
+		
+		given()
+		.log().all()
+		.auth().basic("admin", "senha")//passando usuario e senha para acesso no browser
+	.when()
+		.get("http://restapi.wcaquino.me/basicauth")
+	.then()
+		.log().all()
+		.statusCode(200)
+		.body("status", is("logado"))
+		
+		;	
+		
+	}
+	
+	@Test
+	public void DeveFazerAutenticacaoBasicaChallenge() {
+		
+		given()
+		.log().all()
+		.auth().preemptive().basic("admin", "senha")//passando usuario e senha para acesso no browser
+	.when()
+		.get("http://restapi.wcaquino.me/basicauth2")
+	.then()
+		.log().all()
+		.statusCode(200)
+		.body("status", is("logado"))
+		
+		;	
+		
+	}
 
 }
