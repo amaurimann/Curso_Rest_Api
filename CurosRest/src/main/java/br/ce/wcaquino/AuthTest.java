15948b8b122f36e6a6e66702049206ba32c67540
Amauri Mann <amaurimoraismann@gmail.com>
2020-10-06 23:23:07 -0300

    Acessando API pública com Chave

--- CurosRest/src/main/java/br/ce/wcaquino/AuthTest.java
+++ CurosRest/src/main/java/br/ce/wcaquino/AuthTest.java
@@ -1,6 +1,7 @@
 package br.ce.wcaquino;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
@@ -20,6 +21,29 @@ public class AuthTest {
 			.log().all()
 			.statusCode(200)
 			.body("name", is("Luke Skywalker"))
+		
+		;
+	}
+	
+	//0b9f4497ef28f6a3fa756df8dbf10b69 api key
+	//http://api.openweathermap.org/data/2.5/weather?q=Fortaleza,BR&appid=0b9f4497ef28f6a3fa756df8dbf10b69&&units=metric
+	
+	@Test
+	public void deveObterClima() {
+		
+		given()
+			.log().all()
+			.queryParam("q", "Fortaleza,BR")
+			.queryParam("appid", "0b9f4497ef28f6a3fa756df8dbf10b69")
+			.queryParam("units", "metric")
+		.when()
+			.get("http://api.openweathermap.org/data/2.5/weather")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Fortaleza"))
+			.body("coord.lon", is(-38.52f))
+			.body("main.temp", greaterThan(25f))
 		
 		;
 		
